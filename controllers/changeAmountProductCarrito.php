f7fc5ca91f49dc0698233d5b772c83cbdcd02325
josman <aguirrejosman15@gmail.com>
2021-04-12 09:22:50 -0500

    avance carrito, con las cantidades del producto

--- controllers/changeAmountProductCarrito.php
+++ controllers/changeAmountProductCarrito.php
@@ -1,6 +1,15 @@
 <?php
 require_once "../models/Carrito.php";
-$Carrito = new Carrito();
-$Carrito->changeAmountProduct(37, 2);
-$product = $Carrito->getCurrentProduct(37);
+session_start();
+$carrito = new Carrito();
+$product = $carrito->getCurrentProduct($_GET['product_id']);
+$productAmount = $product['amount'];
+if (2 == $_GET['subOrPlus']) {
+    $carrito->addAmountProduct($_GET['product_id']);
+    echo $productAmount + 1;
+} elseif (1 == $_GET['subOrPlus']) {
+   $carrito->subAmountProduct($_GET['product_id']);
+   echo $productAmount - 1;
+}
+
 ?>
--- views/carritoCompras.php
+++ views/carritoCompras.php
@@ -42,24 +42,35 @@ $products = $productsCarrito->getCurrentArticules($_SESSION['id']);
                     ?>
                     <tr style="font-size: 20px;">
                         <th style=" font-weight: normal"> <?= $inCarrito['product'] ?></th>
-                        <th style=" font-weight: normal" class="text-danger">$ <?= number_format($inCarrito['purchase'], 2, ",", ".") ?></th>
+                        <th style=" font-weight: normal" class="text-danger" >
+                            $ <?= number_format($inCarrito['purchase'], 2, ",", ".") ?></th>
                         <th>
                             <div class="btn-group" role="group" aria-label="Basic outlined example">
-                                <button type="button" class="btn btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                <button type="button" class="btn btn-outline-dark"
+                                        onclick="subAmountProduct(<?= $inCarrito['id'] ?>)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-dash-circle" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                         <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-                                    </svg></button>
-                                <button type="button" class="btn btn-outline-dark" disabled><?= $inCarrito['amount'] ?></button>
-                                <button type="button" class="btn btn-outline-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                                    </svg>
+                                </button>
+                                <button type="button" id="amountProduct" class="btn btn-outline-dark"
+                                        disabled><?= $inCarrito['amount'] ?></button>
+                                <button type="button" class="btn btn-outline-dark"
+                                        onclick="plusAmountProduct(<?= $inCarrito['id'] ?>)">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         class="bi bi-plus-circle" viewBox="0 0 16 16">
                                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                                    </svg></button>
+                                    </svg>
+                                </button>
                             </div>
                         </th>
                         <th>
                             <button type="submit" class="btn"
                                     onclick="deleteRow(this, <?= $inCarrito['id'] ?>)">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" fill="currentColor"
+                                     class="bi bi-x-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                 </svg>
@@ -73,21 +84,35 @@ $products = $productsCarrito->getCurrentArticules($_SESSION['id']);
         </div>
         <div class="col-md-4">
             <p>Su saldo actual es: </p>
-            <p style="color: #bdbdbd; font-size: 14px ">Si gustas puedes seguir añadiendo articulos a tu carrito, tenemos los mejores articulos al mejor precio.</p>
-           <h1 class="text-center" style="background-color: #f9f9f9">Total: 2000</h1>
-           <div class="d-grid gap-2 m-md-5"><button type="button" style="font-size: 25px" class="btn btn-danger">Realizar compra <svg xmlns="http://www.w3.org/2000/svg" width="26" fill="currentColor" class="bi bi-cash-stack" viewBox="0 0 16 16">
-                       <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
-                       <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z"/>
-                   </svg></button>
-           <a type="button" href="tienda.php" style="font-size: 20px" class="mt-md-3 btn btn-outline-secondary">Seguir comprando <svg xmlns="http://www.w3.org/2000/svg" width="24" fill="currentColor" class="bi bi-bag-plus" viewBox="0 0 16 16">
-                       <path fill-rule="evenodd" d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
-                       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-                   </svg></a>
-               <a type="button" href="../controllers/deleteAllFromCarrito.php" style="font-size: 20px" class=" mt-md-3 btn btn-outline-dark">Vaciar Carrito <svg xmlns="http://www.w3.org/2000/svg" width="22" fill="currentColor" class="bi bi-cart-x" viewBox="0 0 16 16">
-                       <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793 7.354 5.646z"/>
-                       <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-                   </svg></a>
-           </div>
+            <p style="color: #bdbdbd; font-size: 14px ">Si gustas puedes seguir añadiendo articulos a tu carrito,
+                tenemos los mejores articulos al mejor precio.</p>
+            <h1 class="text-center" style="background-color: #f9f9f9">Total: 2000</h1>
+            <div class="d-grid gap-2 m-md-5">
+                <button type="button" style="font-size: 25px" class="btn btn-danger">Realizar compra
+                    <svg xmlns="http://www.w3.org/2000/svg" width="26" fill="currentColor" class="bi bi-cash-stack"
+                         viewBox="0 0 16 16">
+                        <path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/>
+                        <path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z"/>
+                    </svg>
+                </button>
+                <a type="button" href="tienda.php" style="font-size: 20px" class="mt-md-3 btn btn-outline-secondary">Seguir
+                    comprando
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" fill="currentColor" class="bi bi-bag-plus"
+                         viewBox="0 0 16 16">
+                        <path fill-rule="evenodd"
+                              d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"/>
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+                    </svg>
+                </a>
+                <a type="button" href="../controllers/deleteAllFromCarrito.php" style="font-size: 20px"
+                   class=" mt-md-3 btn btn-outline-dark">Vaciar Carrito
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" fill="currentColor" class="bi bi-cart-x"
+                         viewBox="0 0 16 16">
+                        <path d="M7.354 5.646a.5.5 0 1 0-.708.708L7.793 7.5 6.646 8.646a.5.5 0 1 0 .708.708L8.5 8.207l1.146 1.147a.5.5 0 0 0 .708-.708L9.207 7.5l1.147-1.146a.5.5 0 0 0-.708-.708L8.5 6.793 7.354 5.646z"/>
+                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>
+                </a>
+            </div>
         </div>
     </div>
 </div>
@@ -113,10 +138,35 @@ $products = $productsCarrito->getCurrentArticules($_SESSION['id']);
                     }
                 }
             }
-
         );
 
     }
+
 </script>
+<script type="text/javascript">
+    function plusAmountProduct(productId) {
+        $.ajax(
+            {
+                url: '../controllers/changeAmountProductCarrito.php?product_id=' + productId + '&subOrPlus=2',
+                success: function (data) {
+                    $("#amountProduct").html(data)
+                }
+            }
+        );
+    }
+</script>
+<script type="text/javascript">
+    function subAmountProduct(productId) {
+        $.ajax(
+            {
+                url: '../controllers/changeAmountProductCarrito.php?product_id=' + productId + '&subOrPlus=1',
+                success: function (data) {
+                    $("#amountProduct").html(data)
+                }
+            }
+        );
+    }
+</script>
+
 </body>
 </html>
